0c0cd0644926115c6aae6c650f883fc9b8936118
Michael Kim <MichaelKim0717@gmail.com>
2018-10-02 21:19:15 -0400

    Added validation, Changed skeleton of sign in page

--- public/app.js
+++ public/app.js
@@ -4,7 +4,7 @@
     $(document).ready(function(){
         $('.tabs').tabs();
 
-    /* Populating all the data within the directory and birthdays */
+    /* Populating all the data within the directory and birthdays with an AJAX call */
         $.getJSON('/directory', function(data) {
             console.log(data);
             for (var i = 0; i < data.length; i++) {
@@ -23,8 +23,15 @@
         });
 
     /* ------------------- JAVASCRIPT FOR DIRECTORY TAB ------------------- */
-        $('.datepicker').datepicker();
+    /* Allows the dropdown menus to be triggerable. */
+        $('.sort-trigger').dropdown();
+        $('.gradepicker').dropdown();
 
+    /* 
+        When a dropdown grade is clicked (not within the addStudent Modal), 
+        Change the text on the dropdown menu button
+        Append all information with the corresponding dropdown item
+    */    
         $(".dropdownGrade").on("click", function(){
             const selectedGrade = $(this).attr("data-grade");
 
@@ -46,109 +53,114 @@
                         "</tr>"
                     );
                 }
-                $(".preloader-wrapper").css("visibility", "hidden");
             });
         });
 
+    /*
+        When a dropdown grade is clicked (within the add/editStudent Modal),
+        Put the selected grade into the corresponding modal field
+    */
         $(".aSGradeV").on("click", function(){
-            console.log($(this).attr("data-grade"));
             $("#aSGrade").val($(this).attr("data-grade"));
             $("#eSGrade").val($(this).attr("data-grade"));
         });
 
-        $('input.autocomplete').autocomplete({
-            data: {
-                /* Add More Values Based On What Was Entered Previously */
-                "Hackensack, NJ" : null,
-                "Teaneck, NJ" : null,
-                "Fort Lee, NJ" : null,
-                "Fair Lawn, NJ" : null,
-                "Garfield, NJ" : null,
-                "Englewood, NJ" : null,
-                "Bergenfield, NJ" : null,
-                "Paramus, NJ" : null,
-                "Mahwah, NJ" : null,
-                "Ridgewood, NJ" : null,
-                "Lodi, NJ" : null,
-                "Cliffside Park, NJ" : null,
-                "Lyndhurst, NJ" : null,
-                "Palisades Park, NJ" : null,
-                "Elmwood Park, NJ" : null,
-                "Rutherford, NJ" : null,
-                "Dumont, NJ" : null,
-                "Wyckoff, NJ" : null,
-                "New Milford, NJ" : null,
-                "North Arlington, NJ" : null,
-                "Tenafly, NJ" : null,
-                "Ramsey, NJ" : null,
-                "Fairview, NJ" : null,
-                "Saddle Brook, NJ" : null,
-                "Oakland, NJ" : null,
-                "Ridgefield Park, NJ" : null,
-                "Hasbrouck Heights, NJ" : null,
-                "Glen Rock, NJ" : null,
-                "Edgewater, NJ" : null,
-                "Wayne, NJ" : null
+    /* Activating datepicker to be used within the addStudent Modal */
+        $('.datepicker').datepicker();
+
+    /* 
+        Receive the list of towns previously entered through other students 
+        The list of towns now becomes a list that can be accessed through autocompletion for the add/editStudent Modal
+    */
+        $.getJSON('/directory', function(data) {
+            let autoCompleteTowns = {};
+            for (var i = 0; i < data.length; i++) {
+                const town = data[i].location;
+                autoCompleteTowns[town] = null;
             }
+            $('input.autocomplete').autocomplete({ data : autoCompleteTowns });
         });
 
+    /*  
+        When the Add Student Button is clicked,
+        Receive entered values
+        Validate values
+        If values are properly entered
+            Perform an AJAX call to add the student into the database
+            Clear the values so that another student can be entered
+            Update the table
+        Else
+            TODO : Let the user know that there was some incorrect information entered
+    */
         $("#addStudentButton").on("click", function(event){
             event.preventDefault();
 
-            const name = $("#aSName").val().trim()
-            const grade = $("#aSGrade").val().trim()
-            const birthday = $("#aSBirthday").val().trim()
-            const location = $("#aSLocation").val().trim()
+            const name = $("#aSName").val().trim();
+            const grade = $("#aSGrade").val().trim();
+            const birthday = $("#aSBirthday").val().trim();
+            const location = $("#aSLocation").val().trim();
 
-            $.ajax({
-                method: "POST",
-                url: "/all/students",
-                data: {
-                    name: name,
-                    grade: grade,
-                    birthday: birthday,
-                    location: location
-                }
-            });
+            if (validateFields(grade, birthday, location)){
+                $.ajax({
+                    method: "POST",
+                    url: "/all/students",
+                    data: {
+                        name: name,
+                        grade: grade,
+                        birthday: birthday,
+                        location: location
+                    }
+                });
+    
+                $("#aSName").val("");
+                $("#aSGrade").val("");
+                $("#aSBirthday").val("");
+                $("#aSLocation").val("");
+    
+                $("#studentTable").empty();
+            
+                $.getJSON('/directory', function(data) {
+                    console.log(data);
+                    for (var i = 0; i < data.length; i++) {
+                        $("#studentTable").append(
+                            "<tr>" +
+                                "<td>" + data[i].name + "</td>" + 
+                                "<td>" + data[i].grade + "</td>" +
+                                "<td>" + data[i].birthday + "</td>" +
+                                "<td>" + data[i].location + "</td>" +
+                                "<td> <button class='btn modal-trigger editStudent' data-value=" + "edit" + data[i]._id + " data-target='editStudentModal'> EDIT </button> </td>" +
+                                "<td> <button class='btn deleteStudent' data-value=" + "delete" + data[i]._id + " id='delete'> DELETE </button> </td>" +
+                            "</tr>"
+                        );
+                    }
+                });
+            }
+            else{
+                $("#asGuideline").html("* : can be empty <br><br> ERROR IN ADDING STUDENT: Please follow the correct formats");
+                console.log($("#asGuideline").text());
+            }
 
-            $("#aSName").val("");
-            $("#aSGrade").val("");
-            $("#aSBirthday").val("");
-            $("#aSLocation").val("");
-
-            $("#studentTable").empty();
-        
-            $.getJSON('/directory', function(data) {
-                console.log(data);
-                for (var i = 0; i < data.length; i++) {
-                    $("#studentTable").append(
-                        "<tr>" +
-                            "<td>" + data[i].name + "</td>" + 
-                            "<td>" + data[i].grade + "</td>" +
-                            "<td>" + data[i].birthday + "</td>" +
-                            "<td>" + data[i].location + "</td>" +
-                            "<td> <button class='btn modal-trigger editStudent' data-value=" + "edit" + data[i]._id + " data-target='editStudentModal'> EDIT </button> </td>" +
-                            "<td> <button class='btn deleteStudent' data-value=" + "delete" + data[i]._id + " id='delete'> DELETE </button> </td>" +
-                        "</tr>"
-                    );
-                }
-            });
         });
 
+    /*  
+        When the edit student button is clicked,
+        TODO : Find the specific student's information
+        TODO : Enter it into the corresponding fields
+    */
         $(document).on("click", ".editStudent", function(event){
             event.preventDefault();
 
             const id = $(this).attr("data-value").substring(4);
             console.log(id);
 
-            /* $.getJSON('/edit/students/' + id, function(data) {
+            $.getJSON('/edit/students/' + id, function(data) {
                 console.log(data);
                 
-                $("#eSName").val(data[0].name);
+                /* $("#eSName").val(data[0].name);
                 $("#eSGrade").val(data[0].grade);
                 $("#eSBirthday").val(data[0].birthday);
-                $("#eSLocation").val(data[0].location);
-            }); */
+                $("#eSLocation").val(data[0].location); */
+            });
 
             /* $.ajax({
                 method: "GET",
@@ -159,6 +171,11 @@
             }); */
         });
 
+    /* 
+        When the delete student button is clicked,
+        Delete the student from the database
+        Update the table
+    */
         $(document).on("click", ".deleteStudent", function(event){
             event.preventDefault();
 
@@ -188,20 +205,17 @@
             });
         });
 
-        /* Allows the dropdown menus to be triggerable. */
-        $('.sort-trigger').dropdown();
-        $('.gradepicker').dropdown();
-
     /* ------------------- JAVASCRIPT FOR BIRTHDAYS TAB ------------------- */
 
     /* Get current month and display it within the birthdays tab */
+
+    /* Get the current date and current month */
         const currDate = (new Date).getMonth();
         $("#currMonth").text(convertMonth(currDate));
-
         const monthAbv = convertMonth(currDate).substring(0, 3);
 
+    /* Receive student information that corresponds to the current month and update the table */
         $("#birthdayTable").empty();
-
         $.getJSON('/birthday/' + monthAbv, function(data) {
             console.log(data);
             for (var i = 0; i < data.length; i++) {
@@ -216,7 +230,10 @@
             }
         });
 
-    /* Change months according to what navigation button was pressed */
+    /* 
+        Change months according to what navigation button was pressed 
+        Update the table according to the new month
+    */
         $("#lastMonth").on("click", function(){
             var selectedMonth = $("#currMonth").text();
             var finalMonthValue = getCurrMonth(selectedMonth) - 1;
@@ -273,6 +290,18 @@
         $('.modal').modal();
     });
 
+
+    /* Validate fields in the add/editStudent Modal so that the correct values are being entered for students */
+    function validateFields(grade, birthday, location){
+        const gradeRegex = /([6-9]|1[0-2])[BG]/;
+        const birthdayRegex = /((Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s([0-9]|[0-2][0-9]|3[0-1]),\s([1-2][0|9][0-9][0-9]))|(^$)/;
+        const locationRegex = /(.+,\s([A-Z][A-Z]))|(^$)/;
+        if (grade.match(gradeRegex) && birthday.match(birthdayRegex) && location.match(locationRegex)){
+            return true;
+        }
+        return false;
+    }
+
     /* Gets the current month and converts it into a string. */
     function convertMonth(monthValue) {
         switch(monthValue) {
@@ -303,6 +332,7 @@
         }
     }
 
+    /* Gets the current month string and returns an integer. */
     function getCurrMonth(monthString) {
         switch(monthString) {
             case "January" :
